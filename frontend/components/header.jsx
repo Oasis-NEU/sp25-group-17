a5efeae6ed0d0fa417df0774a3bbe252a90e422a
KalinaM11 <kalina.monova@gmail.com>
2025-02-04 20:18:49 -0500

    components + header

---
+++ frontend/components/header.jsx
@@ -0,0 +1,37 @@
+
+
+// export default function Header() {
+//   return (
+//     <div className={styles.container}>
+//       <Image
+//         className={styles.logo}
+//         src=""
+//         alt="logo"
+//         width={150}
+//         height={150}
+//       />
+//       <div className={styles.textContainer}>
+//         <h1 className={styles.title}>NU Sci Magazine</h1>
+//         <h2>Northeastern University’s Student-Run Science Magazine</h2>
+//         <div className={styles.pageContainer}>
+//           <Link href={"hello"}>
+//             <p className={styles.pageTitle}>Home</p>
+//           </Link>
+//           <Link href={"bye"}>
+//             <p className={styles.pageTitle}>Print Issues</p>
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// const styles = {
+//   container: "flex items-center w-full py-4",
+//   logo: "border-0",
+//   textContainer: "ml-16",
+//   pageContainer: "flex mt-2",
+//   title: "uppercase text-5xl py-2",
+//   pageTitle:
+//     "mr-10 text-black text-lg border-b-2 border-black hover:border-b-2 hover:border-gray-200 hover:ease-in hover:duration-500",
+// };
